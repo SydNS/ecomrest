1e0840f7cf506018e1ea5e4034ee24209bb23d5e
sydneynsiimbe <sydneynsiimbe@gmail.com>
2022-04-01 17:54:50 +0300

    starting

---
+++ src/test/java/com/example/ecomrest/EcomrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecomrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
