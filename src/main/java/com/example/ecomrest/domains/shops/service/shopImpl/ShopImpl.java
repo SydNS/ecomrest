1ea42406563708fa0a7768f0091d00832a2254c3
sydneynsiimbe <sydneynsiimbe@gmail.com>
2022-04-04 19:33:27 +0300

    implementing the abstract busness logic methods

---
+++ src/main/java/com/example/ecomrest/domains/shops/service/shopImpl/ShopImpl.java
@@ -0,0 +1,8 @@
+package com.example.ecomrest.domains.shops.service.shopImpl;
+
+import com.example.ecomrest.domains.shops.service.ShopService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShopImpl implements ShopService {
+}
